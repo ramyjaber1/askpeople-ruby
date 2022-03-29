c3c2852927de7215f55a75a8bac665a4cf340966
Ramy Jaber <rami.ishtari@gmail.com>
2022-03-29 13:59:58 +0300

    install devise make it work with turbo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   resources :inboxes
   root 'static_pages#landing_page'
   get 'pricing', to: 'static_pages#pricing'
