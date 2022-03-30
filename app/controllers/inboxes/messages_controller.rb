6360e477200187258e0aefd0426addd6e747e087
Ramy Jaber <rami.ishtari@gmail.com>
2022-03-30 16:26:39 +0300

    messages_count

--- app/controllers/inboxes/messages_controller.rb
+++ app/controllers/inboxes/messages_controller.rb
@@ -50,7 +50,7 @@ class Inboxes::MessagesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_message
-      @message = Message.find(params[:id])
+      @message = @inbox.messages.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
